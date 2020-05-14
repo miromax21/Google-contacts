e8e89d39aa30e522ef7eaf8ed1cd490930066d03
Максим Миронов <miromax21@gmail.com>
2020-05-14 19:02:44 +0400

    GoogleContactsUseCase

--- ContactsApp/UseCases/useCases/GoogleContactsUseCase.swift
+++ ContactsApp/UseCases/useCases/GoogleContactsUseCase.swift
@@ -13,29 +13,27 @@ import Moya
 
 class GoogleContactsUseCase  {
     var service: NetworkServiceProtocol!
-    var provider : MoyaProvider<GoogleMoyaService>!
     
     init(service: NetworkServiceProtocol!) {
         self.service = service
-        self.provider = MoyaProvider<GoogleMoyaService>()
     }
-
-    func start() -> Observable<[Entry]?>{
+   func start() -> Observable<[Entry]?>{
         guard
             let googleAccessTokken = UserDataWrapper.googleAccessTokken,
             let email = UserDataWrapper.email,
             let tokkenExpired = UserDataWrapper.googleAccessTokkenExpired,
-            tokkenExpired.timeIntervalSince1970 > NSDate().timeIntervalSince1970,
-            googleAccessTokken != ""
-        else {
-            return Observable.error(RequestError.authentification)
-        }
-
+            tokkenExpired.timeIntervalSince1970 > NSDate().timeIntervalSince1970
+            else {
+                return Observable.error(RequestError.authentification)
+            }
+    
         return Observable.deferred {
             return self.service.request(path: .Contacts(token: googleAccessTokken, userEmail: email) )
                 .flatMap { (userData)  -> Observable<[Entry]?>  in
                     return Observable.of(userData?.feed?.entry)
-                }
+            }
         }
     }
+
+    
 }
